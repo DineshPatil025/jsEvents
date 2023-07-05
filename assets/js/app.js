9d235a71c7639090cfd22cb9ef1f49687d94b4f9
DineshPatil025 <dineshbpatil74@gmail.com>
2023-07-05 13:24:33 +0530

    Updated on select change color

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+const cl = console.log;
+
+const selectColor = document.getElementById('selectColor')
+const allDivs = [...document.querySelectorAll('.all')];
+
+const onChangeColor = (eve => {
+    const selectedColor = eve.target.value;
+    cl(selectedColor)
+
+    allDivs.forEach(div => {
+        div.classList.add('d-none')
+    });
+
+    const showDivs = [...document.getElementsByClassName(selectedColor)]
+    showDivs.forEach(div => {
+        div.classList.remove('d-none')
+    })
+})
+
+
+selectColor.addEventListener("change", onChangeColor)
